6e2bc6f2670166226a2885da9735d39ae5ae9005
alex-p14 <alex-p14@bk.ru>
2022-04-05 19:08:29 +0300

    Edit file brain_games/scripts/brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,55 +1,13 @@
 #!usr/bin/env python3
-from random import randint
-from brain_games.general_logic import (
-    check_user_answer,
-    ask_question,
-    welcome_user,
-    finish_the_game
+from brain_games.engine import run_game
+from brain_games.games.gcd import (
+    get_description,
+    get_question_and_answer
 )
 
 
-def get_remainder(num_1, num_2):
-    return num_1 % num_2
-
-
-def find_gcd(num_1, num_2):
-    if num_2 == 0:
-        return num_1
-    else:
-        return find_gcd(num_2, get_remainder(num_1, num_2))
-
-
-def find_larged_num(num_1, num_2):
-    if num_1 >= num_2:
-        return (num_1, num_2)
-    else:
-        return (num_2, num_1)
-
-
-def get_gcd(num_1, num_2):
-    (max_num, min_num) = find_larged_num(num_1, num_2)
-    result = find_gcd(max_num, min_num)
-    return str(result)
-
-
-def begin_brain_gcd(user_name, num_of_repeat=1):
-    if num_of_repeat > 3:
-        finish_the_game(user_name)
-    else:
-        num_1 = randint(1, 100)
-        num_2 = randint(1, 100)
-        question = f"{num_1} {num_2}"
-        user_answer = ask_question(question)
-        right_answer = get_gcd(num_1, num_2)
-        if check_user_answer(user_answer, right_answer, user_name):
-            num_of_repeat += 1
-            return begin_brain_gcd(user_name, num_of_repeat)
-
-
 def main():
-    user_name = welcome_user()
-    print("Find the greatest common divisor of given numbers.")
-    begin_brain_gcd(user_name)
+    run_game(get_description, get_question_and_answer)
 
 
 if __name__ == 'main':
