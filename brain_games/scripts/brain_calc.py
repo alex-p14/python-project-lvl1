4ece8db67cbe5b2a16cfd137a6022b69ae94eddd
alex-p14 <alex-p14@bk.ru>
2022-04-05 19:07:41 +0300

    Edit file brain_games/scripts/brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,42 +1,13 @@
 #!usr/bin/env python3
-from random import randint, choice
-from brain_games.general_logic import (
-    check_user_answer,
-    ask_question,
-    welcome_user,
-    finish_the_game
+from brain_games.engine import run_game
+from brain_games.games.calc import (
+    get_description,
+    get_question_and_answer
 )
 
 
-def find_expression_result(num_1, num_2, operator):
-    if operator == '+':
-        result = num_1 + num_2
-    elif operator == '-':
-        result = num_1 - num_2
-    else:
-        result = num_1 * num_2
-    return str(result)
-
-
-def begin_brain_calc(user_name, num_of_repeat=1):
-    if num_of_repeat > 3:
-        finish_the_game(user_name)
-    else:
-        num_1 = randint(1, 100)
-        num_2 = randint(1, 100)
-        operator = choice('+-*')
-        question = f"{num_1} {operator} {num_2}"
-        user_answer = ask_question(question)
-        right_answer = find_expression_result(num_1, num_2, operator)
-        if check_user_answer(user_answer, right_answer, user_name):
-            num_of_repeat += 1
-            return begin_brain_calc(user_name, num_of_repeat)
-
-
 def main():
-    user_name = welcome_user()
-    print('What is the result of the expression?')
-    begin_brain_calc(user_name)
+    run_game(get_description, get_question_and_answer)
 
 
 if __name__ == 'main':
