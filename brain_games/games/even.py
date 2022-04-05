22573dde4a89c12ed74c90f91d632caba75941b2
alex-p14 <alex-p14@bk.ru>
2022-04-05 16:11:39 +0300

    Add brain_games/games/even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+def get_description():
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even_number(num):
+    return num % 2 == 0
+
+
+def get_right_answer(question):
+    if is_even_number(question):
+        return "yes"
+    else:
+        return "no"
+
+
+def get_question_and_answer():
+    question = randint(1, 100)
+    answer = get_right_answer(question)
+    return question, answer
