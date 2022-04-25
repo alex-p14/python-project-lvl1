d6c32097c93b5e74adced6207d5ef2fa8cb5bb10
alex-p14 <alex-p14@bk.ru>
2022-04-25 13:19:40 +0300

    Edit file brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,11 +5,7 @@ def get_description():
     return 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def get_right_answer(question):
-    return "no" if question % 2 else "yes"
-
-
 def get_question_and_answer():
     question = random.randint(1, 100)
-    answer = get_right_answer(question)
+    answer = "no" if question % 2 else "yes"
     return question, answer
