6812e88c122b1affa1f25a6d28d97af0d1859d39
alex-p14 <alex-p14@bk.ru>
2022-04-25 13:20:29 +0300

    brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,30 +5,22 @@ def get_description():
     return 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def find_smallest_divisor(num, divisor=2):
+def find_smallest_divisor(num, divisor=3):
     if divisor ** 2 > num:
         return num
     elif num % divisor == 0:
         return divisor
-    return find_smallest_divisor(num, divisor + 1)
+    return find_smallest_divisor(num, divisor + 2)
 
 
 def is_prime_number(num):
-    if num == 1:
+    if num == 1 or (num > 2 and num % 2 == 0):
         return False
     return find_smallest_divisor(num) == num
-
-
-def get_right_answer(question):
-    if is_prime_number(question):
-        answer = "yes"
-    else:
-        answer = "no"
-    return answer
 
 
 def get_question_and_answer():
     num = random.randint(1, 100)
     question = str(num)
-    answer = get_right_answer(num)
+    answer = "yes" if is_prime_number(num) else "no"
     return question, answer
